766fb71b33333b2ddc92488fc0557a1ca2c916c1
RussoFrancesco <franci.russo@icloud.com>
2025-04-29 22:16:19 +0200

    Check quality

--- synthetic_data.py
+++ synthetic_data.py
@@ -1,10 +1,9 @@
 from sdv.single_table import TVAESynthesizer
 from sdv.metadata import Metadata
 import pandas as pd
-from sdmetrics.reports.single_table import QualityReport
+from sdmetrics.column_pairs import StatisticMSAS
 
 df = pd.read_csv("original_dataset.csv")
-report = QualityReport()
 
 metadata = Metadata.detect_from_dataframe(df)
 
@@ -16,8 +15,15 @@ metadata = Metadata.detect_from_dataframe(df)
 #synthesizer.save("tvae_model.pkl")
 synthesizer = TVAESynthesizer.load("tvae_model.pkl")
 
-synthetic_data = synthesizer.sample(num_rows=10000)
+while True:
+    synthetic_data = synthesizer.sample(num_rows=10000)
 
-report.generate(df, synthetic_data, metadata.to_dict()["tables"]["table"])
-if report.get_score >= 0.9:
-    synthetic_data.to_csv("synthetic_data.csv", index=False)
+    result = StatisticMSAS.compute(
+        real_data=(df["Cell"], df["SOH"]),
+        synthetic_data=(synthetic_data["Cell"], synthetic_data["SOH"]),
+        statistic="median"
+    )
+
+    if result >= 0.6:
+        synthetic_data.to_csv("synthetic_data.csv", index=False)
+
