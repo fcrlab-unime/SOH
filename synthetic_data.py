49bfb0f973c9adcd0bf6e7004358c20d460f065f
RussoFrancesco <franci.russo@icloud.com>
2025-04-29 22:09:26 +0200

    Check quality

--- synthetic_data.py
+++ synthetic_data.py
@@ -1,19 +1,23 @@
 from sdv.single_table import TVAESynthesizer
 from sdv.metadata import Metadata
 import pandas as pd
-import numpy as np
+from sdmetrics.reports.single_table import QualityReport
 
 df = pd.read_csv("original_dataset.csv")
+report = QualityReport()
 
 metadata = Metadata.detect_from_dataframe(df)
 
 
-synthesizer = TVAESynthesizer(metadata, verbose=True, epochs=5000)
+#synthesizer = TVAESynthesizer(metadata, verbose=True, epochs=5000)
 
-synthesizer.fit(df)
+#synthesizer.fit(df)
 
-synthesizer.save("tvae_model.pkl")
+#synthesizer.save("tvae_model.pkl")
+synthesizer = TVAESynthesizer.load("tvae_model.pkl")
 
 synthetic_data = synthesizer.sample(num_rows=10000)
 
-synthetic_data.to_csv("synthetic_data.csv", index=False)
+report.generate(df, synthetic_data, metadata.to_dict()["tables"]["table"])
+if report.get_score >= 0.9:
+    synthetic_data.to_csv("synthetic_data.csv", index=False)
